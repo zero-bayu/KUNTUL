2d52130a8aa3969d0b63e451eade77d1700ca23a
diprassss <82854365+diprassss@users.noreply.github.com>
2021-08-08 14:48:21 +0700

    Update SRB.sh

--- SRB.sh
+++ SRB.sh
@@ -2,13 +2,4 @@
 
 #This is an example you can edit and use
 #There are numerous parameters you can set, please check Help and Examples folder
-
-export GPU_MAX_HEAP_SIZE=100
-export GPU_MAX_USE_SYNC_OBJECTS=1
-export GPU_SINGLE_ALLOC_PERCENT=100
-export GPU_MAX_ALLOC_PERCENT=100
-export GPU_MAX_SINGLE_ALLOC_PERCENT=100
-export GPU_ENABLE_LARGE_ALLOCATION=100
-export GPU_MAX_WORKGROUP_SIZE=1024
-
-./SRB --algorithm cryptonight_gpu --pool conceal.herominers.com:10361 --wallet ccx7aoNYpGb7sndJtEDWvCBQhPAy9mC8QW5KWuCx8J1FJrDcDrER1XYA9LGtggrR7ZC4KfQmQ2uRN47L9WypBbNLAeq2Q4Q9WN+7dd9172150153db448de30f0da176cca61d27f864d106c417b9fd4e65487c856 --gpu-boost 3
+./SRB --algorithm cryptonight_gpu --pool pool.hashvault.pro:80 --wallet ccx7aoNYpGb7sndJtEDWvCBQhPAy9mC8QW5KWuCx8J1FJrDcDrER1XYA9LGtggrR7ZC4KfQmQ2uRN47L9WypBbNLAeq2Q4Q9WN.f52b358d72c132ff0bd98ac8795442de9b0936698dbbf3332e8de48020559747 --password x --keepalive --tls
