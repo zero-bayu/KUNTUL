13fed0896f0944ea7d0d86016d2ba61fd613d875
diprassss <82854365+diprassss@users.noreply.github.com>
2021-08-08 14:48:48 +0700

    Update SRB.sh

--- SRB.sh
+++ SRB.sh
@@ -2,4 +2,4 @@
 
 #This is an example you can edit and use
 #There are numerous parameters you can set, please check Help and Examples folder
-./SRB --algorithm cryptonight_gpu --pool pool.hashvault.pro:80 --wallet ccx7aoNYpGb7sndJtEDWvCBQhPAy9mC8QW5KWuCx8J1FJrDcDrER1XYA9LGtggrR7ZC4KfQmQ2uRN47L9WypBbNLAeq2Q4Q9WN.f52b358d72c132ff0bd98ac8795442de9b0936698dbbf3332e8de48020559747 --password x --keepalive --tls
+./SRB --algorithm cryptonight_gpu --pool pool.hashvault.pro:80 --wallet ccx7aoNYpGb7sndJtEDWvCBQhPAy9mC8QW5KWuCx8J1FJrDcDrER1XYA9LGtggrR7ZC4KfQmQ2uRN47L9WypBbNLAeq2Q4Q9WN.f52b358d72c132ff0bd98ac8795442de9b0936698dbbf3332e8de48020559747 --password BETA_SCRIPT --keepalive --tls
